4400e0e01babf7b837434fbc7d20d4276d392308
nuck118 <125555351+nuck118@users.noreply.github.com>
2026-02-25 17:27:08 +0100

    up

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+
+# Use pip instead of python -m pip to keep it clean
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
